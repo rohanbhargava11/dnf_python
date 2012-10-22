cf3aa2daaf521e6dfc28c5339b268c9c43a56d34
Rohan Bhargava <rohan.bhargava11@gmail.com>
2012-10-22 13:36:24 -0300

    reading from camera

--- dnf2d.py
+++ dnf2d.py
@@ -10,7 +10,7 @@ from mpl_toolkits.mplot3d import Axes3D
 import scipy.signal as sp
 import cv2
 
-
+from time import *
 print 'Hello'
     
 nn=101
@@ -103,41 +103,50 @@ I_ext=np.zeros((nn,nn))
 for k in range(int(nn/2-np.floor(nn/20)),int(nn/2+np.floor(nn/20))+1):
     I_ext[k]=1
 '''
+
 I_ext=np.zeros((nn,nn))
-input_image=cv2.imread('/home/rohan/Documents/dnf_python/test1.jpg',0)
-input_image=cv2.resize(input_image,(101,101))
-for i in range (101):
-    for j in range (101):
-        if input_image[i][j]>140:
-            input_image[i][j]=0
-        else:
-            input_image[i][j]=1
-I_ext=input_image#/np.max(input_image)
-#cv2.imshow('test',input_image)
-#I_ext[int(nn/2-np.floor(nn/20)):int(nn/2+np.floor(nn/20))+1,
- 
-#     int(nn/2-np.floor(nn/20)):int(nn/2+np.floor(nn/20))+1] = 1
-#I_ext[50:60,50:60]=1
-#I_ext=gauss_pbc(3*math.pi/2,3*math.pi/2,sig)
-
-
-#!print u.shape
-
-I_ext[10:30,10:30] = 1
-
-time=20
-u_history=np.zeros((nn,nn))
-
-u=np.zeros((nn,nn))
-for k in range(time):
+#cap=cv2.VideoCapture(0)
+while True:
+    #ret,im=cap.read()    
+    #im=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+    input_image=cv2.imread('/home/rohan/Documents/dnf_python/test1.jpg',0)
+    input_image=cv2.resize(input_image,(101,101))
+    for i in range (101):
+        for j in range (101):
+            if input_image[i][j]>140:
+                input_image[i][j]=0
+            else:
+                input_image[i][j]=1
+    I_ext=input_image#/np.max(input_image)
+    #cv2.imshow('test',input_image)
+    #I_ext[int(nn/2-np.floor(nn/20)):int(nn/2+np.floor(nn/20))+1,
+     
+    #     int(nn/2-np.floor(nn/20)):int(nn/2+np.floor(nn/20))+1] = 1
+    #I_ext[50:60,50:60]=1
+    #I_ext=gauss_pbc(3*math.pi/2,3*math.pi/2,sig)
     
+    
+    #!print u.shape
+    
+    #I_ext[10:30,10:30] = 1
+    
+    time=20
+    u_history=np.zeros((nn,nn))
+    
+    u=np.zeros((nn,nn))
+    #for k in range(time):
+        
     u=update(u,I_ext)
     r=1/(1+np.exp(-u))
     u_history=r
-    
-#plot(1,u_history)
+    print 'I am going to plot' 
+    plt.hold(True)
+    plot(1,u_history)
+    plt.show()
+    #sleep()
+    #plt.close()
 #plot(7,I_ext)
-
+'''
 u_history=np.zeros((nn,nn))
 I_ext=np.zeros((nn,nn))
 #I_ext[10:30,10:30] = 1
@@ -164,6 +173,7 @@ for k in range(time):
     u_history=r
     
 plot(3,u_history)
+'''
 '''
 time =20
 I_ext=np.zeros((nn,nn))
@@ -204,7 +214,7 @@ for k in range(time):
     u_history=r
 plot(5,u_history)
 '''
-plot(6,w)
+#plot(6,w)
 
 
 plt.show()
