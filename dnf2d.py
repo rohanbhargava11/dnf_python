512b6eba3dad4dc51b16a67ae91905660ce24486
Rohan Bhargava <rohan.bhargava11@gmail.com>
2012-10-22 14:05:35 -0300

    only one packet can survive made for the experiment

--- dnf2d.py
+++ dnf2d.py
@@ -10,20 +10,20 @@ from mpl_toolkits.mplot3d import Axes3D
 import scipy.signal as sp
 import cv2
 
-from time import *
-print 'Hello'
+
+
     
 nn=101
 dx=2*math.pi/nn
 dy=2*math.pi/nn
-sig=2*math.pi/15
+sig=2*math.pi/10
 sig1=sig*4#mexican hat
 C=0.09
 pat=np.zeros((nn,nn))
-h=0.0 # Just now it is set to  0.0 later I will give its some input
+h=10.0 # Just now it is set to  0.0 later I will give its some input
 tau_inv=0.1
 
-
+print 'hello world'
 X,Y=np.mgrid[-nn/2:nn/2,-nn/2:nn/2]
 def weights(sig):
     f=np.zeros((nn,nn))    
@@ -103,118 +103,86 @@ I_ext=np.zeros((nn,nn))
 for k in range(int(nn/2-np.floor(nn/20)),int(nn/2+np.floor(nn/20))+1):
     I_ext[k]=1
 '''
-
 I_ext=np.zeros((nn,nn))
-#cap=cv2.VideoCapture(0)
-while True:
-    #ret,im=cap.read()    
-    #im=cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
-    input_image=cv2.imread('/home/rohan/Documents/dnf_python/test1.jpg',0)
-    input_image=cv2.resize(input_image,(101,101))
-    for i in range (101):
-        for j in range (101):
-            if input_image[i][j]>140:
-                input_image[i][j]=0
-            else:
-                input_image[i][j]=1
-    I_ext=input_image#/np.max(input_image)
-    #cv2.imshow('test',input_image)
-    #I_ext[int(nn/2-np.floor(nn/20)):int(nn/2+np.floor(nn/20))+1,
-     
-    #     int(nn/2-np.floor(nn/20)):int(nn/2+np.floor(nn/20))+1] = 1
-    #I_ext[50:60,50:60]=1
-    #I_ext=gauss_pbc(3*math.pi/2,3*math.pi/2,sig)
+input_image=cv2.imread('/home/rohan/Documents/dnf_python/test1.jpg',0)
+input_image=cv2.resize(input_image,(101,101))
+for i in range (101):
+    for j in range (101):
+        if input_image[i][j]>140:
+            input_image[i][j]=0
+        else:
+            input_image[i][j]=1
+I_ext=input_image#/np.max(input_image)
+#cv2.imshow('test',input_image)
+#I_ext[int(nn/2-np.floor(nn/20)):int(nn/2+np.floor(nn/20))+1,
+ 
+#     int(nn/2-np.floor(nn/20)):int(nn/2+np.floor(nn/20))+1] = 1
+#I_ext[50:60,50:60]=1
+#I_ext=gauss_pbc(3*math.pi/2,3*math.pi/2,sig)
+
+
+#!print u.shape
+
+#I_ext[10:20,10:20] = 1
+
+time=20
+u_history=np.zeros((nn,nn))
+
+u=np.zeros((nn,nn))
+for k in range(time):
     
-    
-    #!print u.shape
-    
-    #I_ext[10:30,10:30] = 1
-    
-    time=20
-    u_history=np.zeros((nn,nn))
-    
-    u=np.zeros((nn,nn))
-    #for k in range(time):
-        
     u=update(u,I_ext)
     r=1/(1+np.exp(-u))
     u_history=r
-    print 'I am going to plot' 
-    plt.hold(True)
-    plot(1,u_history)
-    plt.show()
-    #sleep()
-    #plt.close()
-#plot(7,I_ext)
-'''
+    
+plot(1,u_history)
+plot(7,I_ext)
+
 u_history=np.zeros((nn,nn))
 I_ext=np.zeros((nn,nn))
-#I_ext[10:30,10:30] = 1
-time=20
+#I_ext[10:20,10:20] = 1
+time=30
+#h=-15.0
 for k in range(time):
     u=update(u,I_ext)
     r=1/(1+np.exp(-u))
     u_history=r
     
-#plot(2,u_history)
-
+plot(2,u_history)
 u_history=np.zeros((nn,nn))
 I_ext=np.zeros((nn,nn))
-I_ext[10:30,10:30] = 1
-time=20
-h=5.0 # changes the h to control the decay in the bubble without the input
-
-# without this put the time as 8 and you can see the decay
-
-
+I_ext[10:20,10:20] = 1
+time=30
+#h=-15.0
 for k in range(time):
     u=update(u,I_ext)
     r=1/(1+np.exp(-u))
     u_history=r
     
 plot(3,u_history)
-'''
-'''
-time =20
-I_ext=np.zeros((nn,nn))
-u_history=np.zeros((nn,nn))
-for k in range(time):
-    
-    u=update(u,I_ext)
-    r=1/(1+np.exp(-u))
-    u_history=r
-    
-plot(4,u_history)
 
 
-I_ext=np.zeros((nn,nn))
-u_history=np.zeros((nn,nn))
-for k in range(time):
-    
-    u=update(u,I_ext)
-    r=1/(1+np.exp(-u))
-    u_history=r
-    
-plot(4,u_history)
-
-'''
-'''
-I_ext[10:20,10:20] = 1
-time=20
-for k in range(time):
-    u=update(u,I_ext)
-    r=1/(1+np.exp(-u))
-    u_history=r
-plot(4,u_history)
 I_ext=np.zeros((nn,nn))
 time=20
+u_history=np.zeros((nn,nn))
+#h=5
 for k in range(time):
     u=update(u,I_ext)
     r=1/(1+np.exp(-u))
     u_history=r
-plot(5,u_history)
+plot(4,u_history)
+
 '''
-#plot(6,w)
+I_ext=np.zeros((nn,nn))
+time=20
+
+for k in range(time):
+    u=update(u,I_ext)
+    r=1/(1+np.exp(-u))
+    u_history=r
+plot(4,u_history)
+'''
+plot(6,w)
 
 
 plt.show()
